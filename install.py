11e04aec7d4a0680657076094d257321bf1d800d
Matheus Cassiano <matheuscassiano9@gmail.com>
2019-01-30 20:09:55 -0300

    Correção de um comando para o sistema

--- install.py
+++ install.py
@@ -2,5 +2,5 @@ import os
 
 os.system("sudo apt-get install python3-pip")
 os.system("sudo pip3 install geoip2")
-os.system("sudo apt-get install nmap")
+os.system("sudo apt-get install python3-nmap")
 os.system("python3 main.py")
